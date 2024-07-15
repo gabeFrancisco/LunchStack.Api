4bb835d8d229e42bc657d12a1db19ede36d8db6d
gabeFrancisco <gabegit2@gmail.com>
2024-07-14 21:15:24 -0300

    project and controller path setup

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LunchStack.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("God bless you!");
+        }
+    }
+}
