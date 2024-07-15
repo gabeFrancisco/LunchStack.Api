770ab89ef949d742163af38bc35af6e4f56dbefc
gabeFrancisco <gabegit2@gmail.com>
2024-07-14 21:21:27 -0300

    nuget ef packages and AppDbContext config

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunchStack.Api.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
